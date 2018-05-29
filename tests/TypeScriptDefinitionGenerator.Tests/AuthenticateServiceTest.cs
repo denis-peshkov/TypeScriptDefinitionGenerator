de43956c78daf5d12ac97a27f1c87603814898a0
Denis Peshkov <peshkov@bk.ru>
2018-05-29 16:15:46 +0300

    added tests libs

---
+++ tests/TypeScriptDefinitionGenerator.Tests/AuthenticateServiceTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using EnvDTE;
+using NUnit.Framework;
+using System.Windows.Forms;
+
+//using Microsoft.Extensions.Options;
+//using Ploeh.AutoFixture;
+//using Microsoft.AspNetCore.Mvc;
+
+namespace TypeScriptDefinitionGenerator.Tests
+{
+    [TestFixture]
+    public class AuthenticateServiceTest : BaseTestController
+    {
+        public override void SetUp()
+        {
+            base.SetUp();
+        }
+
+        public override void TearDown()
+        {
+            base.TearDown();
+        }
+
+        [Test]
+        public void _ShouldWorkProperly()
+        {
+            //Arrange
+            ProjectItem item = null;
+             
+
+            var proj = EnvDTE.Solution.FindProjectItem(knownFile).ContainingProject;
+
+
+            //Act
+            string dts = GenerationService.ConvertToTypeScript(item);
+            var res = Encoding.UTF8.GetBytes(dts);
+
+            //Assert
+            //Assert.IsNotNull(userId);
+            //Assert.AreEqual(registerDto.Login, userNew.Login);
+        }
+
+    
+        [Test]
+        public void e_ShouldWorkProperly()
+        {
+            //Arrange
+            ProjectItem item = null;
+
+
+            //Act
+            //var list = IntellisenseParser.ProcessFile(item, null);
+            //var res = IntellisenseWriter.WriteTypeScript(list);
+
+            //Assert
+            //Assert.IsNotNull(userId);
+            //Assert.AreEqual(registerDto.Login, userNew.Login);
+        }
+    }
+}
---
+++ tests/TypeScriptDefinitionGenerator.Tests/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace TypeScriptDefinitionGenerator.Tests
+{
+    public class Class1
+    {
+        public string Temp { get; set; }
+
+        public Mas Mas { get; set; }
+
+        public Some Some { get; set; }
+    }
+
+    public enum Mas
+    {
+        First,
+        Next
+    }
+}
