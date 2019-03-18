4f70fa3a6341bd925b8002921e392222d93239a2
Luke Owlclaw2 <2737900+LukeOwlclaw@users.noreply.github.com>
2019-03-18 11:22:25 +0100

    Added test CheckGenerateTypeScriptOutput
    Included new test files

---
+++ tests/ClassLibrary1/base/ThirdClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1      
+{
+    public class ThirdClass     
+    {
+        public int MyProperty { get; set; }
+        public SomeClass Complex1 { get; set; }
+        public SomeSomeClass Complex2 { get; set; }
+    }
+}
+    
